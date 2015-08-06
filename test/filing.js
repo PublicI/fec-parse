8e730546cdfa044652b77f1266cc68880e60a6f5
Chris Zubak-Skees <chriszs@gmail.com>
2015-08-05 21:42:25 -0400

    Janky auto-detect for modern filings with file separator delimiters

--- test/filing.js
+++ test/filing.js
@@ -1,14 +1,16 @@
-var fs = require('fs');
+var fs = require('fs'),
+	_ = require('lodash');
 
 var Filing = require('../');
 
-var filingId = '266598';
+// var filingId = '266598';
+var filingId = '1015585'; // Hillary for America July 2015 report
 /*
 fs.readFile(function (err,contents) {
     var fromFech = JSON.parse(contents);
 */
     new Filing(__dirname + '/temp/' + filingId + '.fec',function (err,filing) {
-        console.log(JSON.stringify(filing,'  '));
+        console.log(JSON.stringify(filing,null,'  '));
     });
 //});
 
