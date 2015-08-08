513b3bab72a04356759a773199287adc3234399a
Chris Zubak-Skees <chriszs@gmail.com>
2015-08-08 12:27:33 -0400

    Unit test framework

---
+++ test/test.js
@@ -0,0 +1,13 @@
+var chai = require('chai'),
+	streamtest = require('streamtest');
+
+chai.should();
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            [1, 2, 3].indexOf(5).should.equal(-1);
+            [1, 2, 3].indexOf(0).should.equal(-1);
+        });
+    });
+});
