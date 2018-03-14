e9b93223218b7f25054a155a0203e0620b1757b3
Chris Zubak-Skees <chriszs@gmail.com>
2018-03-13 22:39:27 -0400

    Add F3 paper rows

--- test/test.js
+++ test/test.js
@@ -117,44 +117,46 @@ describe('parser.js', function() {
                 throw err;
             }
 
-            lines.length.should.equal(9);
+            lines.length.should.equal(10);
             lines[0].record_type.should.equal('HDR');
             lines[0].fec_version.should.equal('P3.2');
             lines[0].soft_name.should.equal('"Aurotech/Captricity"');
             lines[0].batch_number.should.equal('1');
             lines[0].received_date.should.equal('20161008');
 
-            lines[1].form_type.should.equal('SA11AI');
-            lines[1].filer_committee_id_number.should.equal('C00608851');
-            lines[1].contributor_last_name.should.equal('TARRICONE');
-            lines[1].contributor_first_name.should.equal('ANTHONY');
-            lines[1].contributor_street_1.should.equal('22 VINE ST');
-            lines[1].contributor_city.should.equal('GLOUCESTER');
-            lines[1].contributor_state.should.equal('MA');
-            lines[1].contributor_zip_code.should.equal('019301759');
-            lines[1].contribution_date.should.equal('20160630');
-            lines[1].contributor_employer.should.equal('KREINDLER AND KREINDLER LLP');
-            lines[1].contributor_occupation.should.equal('ATTORNEY');
-            lines[1].election_code.should.equal('P2016');
-            lines[1].contribution_aggregate.should.equal('3000.00');
-            lines[1].contribution_amount.should.equal('1500.00');
-            lines[1].memo_text_description.should.equal('* EARMARKED CONTRIBUTION: SEE BELOW');
-            lines[1].image_number.should.equal('201610040200394419');
+            lines[1].begin_image_number.should.equal('201610040200394414');
 
-            lines[6].form_type.should.equal('SB17');
-            lines[6].filer_committee_id_number.should.equal('C00608851');
-            lines[6].payee_last_name.should.equal('HEAD');
-            lines[6].payee_first_name.should.equal('BRIGHT');
-            lines[6].payee_street_1.should.equal('225 ANNUNCIATION ST');
-            lines[6].payee_city.should.equal('LAFAYETTE');
-            lines[6].payee_state.should.equal('LA');
-            lines[6].payee_zip_code.should.equal('705086027');
-            lines[6].expenditure_date.should.equal('20160401');
-            lines[6].expenditure_purpose_descrip.should.equal('CONSULTING: COMMUNICATIONS');
-            lines[6].category_code.should.equal('001');
-            lines[6].election_code.should.equal('P2016');
-            lines[6].expenditure_amount.should.equal('4000.00');
-            lines[6].image_number.should.equal('201610040200394555');
+            lines[2].form_type.should.equal('SA11AI');
+            lines[2].filer_committee_id_number.should.equal('C00608851');
+            lines[2].contributor_last_name.should.equal('TARRICONE');
+            lines[2].contributor_first_name.should.equal('ANTHONY');
+            lines[2].contributor_street_1.should.equal('22 VINE ST');
+            lines[2].contributor_city.should.equal('GLOUCESTER');
+            lines[2].contributor_state.should.equal('MA');
+            lines[2].contributor_zip_code.should.equal('019301759');
+            lines[2].contribution_date.should.equal('20160630');
+            lines[2].contributor_employer.should.equal('KREINDLER AND KREINDLER LLP');
+            lines[2].contributor_occupation.should.equal('ATTORNEY');
+            lines[2].election_code.should.equal('P2016');
+            lines[2].contribution_aggregate.should.equal('3000.00');
+            lines[2].contribution_amount.should.equal('1500.00');
+            lines[2].memo_text_description.should.equal('* EARMARKED CONTRIBUTION: SEE BELOW');
+            lines[2].image_number.should.equal('201610040200394419');
+
+            lines[7].form_type.should.equal('SB17');
+            lines[7].filer_committee_id_number.should.equal('C00608851');
+            lines[7].payee_last_name.should.equal('HEAD');
+            lines[7].payee_first_name.should.equal('BRIGHT');
+            lines[7].payee_street_1.should.equal('225 ANNUNCIATION ST');
+            lines[7].payee_city.should.equal('LAFAYETTE');
+            lines[7].payee_state.should.equal('LA');
+            lines[7].payee_zip_code.should.equal('705086027');
+            lines[7].expenditure_date.should.equal('20160401');
+            lines[7].expenditure_purpose_descrip.should.equal('CONSULTING: COMMUNICATIONS');
+            lines[7].category_code.should.equal('001');
+            lines[7].election_code.should.equal('P2016');
+            lines[7].expenditure_amount.should.equal('4000.00');
+            lines[7].image_number.should.equal('201610040200394555');
 
             done();
         });
