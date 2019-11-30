667c010068d254da92fd0e27408d93dcf3b04e20
Chris Zubak-Skees <chriszs@gmail.com>
2019-11-30 14:59:26 -0500

    Resolve UMD globals

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import builtins from '@stream-io/rollup-plugin-node-builtins';
 import resolve from 'rollup-plugin-node-resolve';
+import globals from 'rollup-plugin-node-globals';
 
 const config = {
 	input: 'index.js',
@@ -10,7 +11,7 @@ const config = {
 		format: 'umd',
 		name: 'FECParse'
 	},
-	plugins: [commonjs(), json(), builtins(), resolve()]
+	plugins: [commonjs(), json(), globals(), builtins(), resolve()]
 };
 
 export default config;
